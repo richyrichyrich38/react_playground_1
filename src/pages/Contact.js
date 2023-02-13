0d25b22bb3640fde77c8c60b625027058ab63f26
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-02-13 10:11:27 +0000

    private styles overview

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,29 @@
+const styles = {
+  header: {
+    textAlign: 'center',
+    margin: '20px 0'
+  },
+
+  submitBtn: {
+    backgroundColor: 'lightseagreen',
+    color: 'white',
+    cursor: 'pointer'
+  }
+};
+
 function Contact() {
   return (
-    <h1>Contact</h1>
+    <>
+      <h1 style={styles.header}>Contact me</h1>
+
+      <form action="">
+        <input type="text" placeholder="Enter your name" />
+        <input type="text" placeholder="Enter your email address" />
+        <textarea placeholder="Enter your message" cols="30" rows="10"></textarea>
+        <button style={styles.submitBtn}>Submit</button>
+
+      </form>
+    </>
   )
 }
 
